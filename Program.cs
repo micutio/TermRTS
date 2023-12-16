8fc94cf068227529abe4e80c4a5739dfe3e3ec5e
micutio <wagner.mchl@googlemail.com>
2023-12-16 09:44:37 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleRenderer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var canvas = new ConsoleCanvas()
+            .CreateBorder()
+            .Render();
+
+        Console.ReadKey();
+    }
+}
