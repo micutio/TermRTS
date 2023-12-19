b4963e1089e9287df0742d738cb41878f2fce389
Michael Wagner <michael-a.wagner@rohde-schwarz.com>
2023-12-19 17:21:24 +0800

    extract input handling into Input class

--- termRTS.Runner/Program.cs
+++ termRTS.Runner/Program.cs
@@ -1,50 +1,26 @@
-﻿using System.Threading.Channels;
-using ConsoleRenderer;
+﻿using ConsoleRenderer;
+using termRTS.Engine;
 
-namespace termRTS.Runner
+namespace termRTS.Runner;
+
+internal static class Program
 {
-    internal static class Program
+    private static async Task Main(/* string[] args */)
     {
-        private static async Task Main(/* string[] args */)
+        // main thread reads messages from the channel
+        Console.CursorVisible = false;
+        var canvas = new ConsoleCanvas()
+            .CreateBorder()
+            .Render();
+
+        var consoleInput = new Input();
+        consoleInput.Run();
+
+        await foreach (var message in consoleInput.KeyEventReader.ReadAllAsync())
         {
-            // Create Channel for strings
-            var channel = Channel.CreateUnbounded<string>();
-
-            var keyInputTask = Task.Run(async () =>
-            {
-                var writer = channel.Writer;
-                var keepRunning = true;
-                while (keepRunning)
-                {
-                    if (!Console.KeyAvailable)
-                        continue;
-
-                    var keyInfo = await Task.Run(() => Console.ReadKey(true));
-                    if (keyInfo.Key == ConsoleKey.Escape)
-                    {
-                        keepRunning = false;
-                    }
-                    await writer.WriteAsync($"key {keyInfo.KeyChar}");
-                }
-                await writer.WriteAsync("closing channel");
-                writer.Complete();
-            });
-
-            // main thread reads messages from the channel
-            Console.CursorVisible = false;
-            var canvas = new ConsoleCanvas()
-                .CreateBorder()
-                .Render();
-
-            var reader = channel.Reader;
-            await foreach (var message in reader.ReadAllAsync())
-            {
-                canvas.Clear().CreateBorder();
-                canvas.Text(2, 2, $"Received: '{message}'");
-                canvas.Render();
-            }
-
-            await keyInputTask;
+            canvas.Clear().CreateBorder();
+            canvas.Text(2, 2, $"Received: {message}");
+            canvas.Render();
         }
     }
 }
