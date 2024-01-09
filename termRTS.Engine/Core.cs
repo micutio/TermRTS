bfdbe8c008fd3970685c06665de5b205a884b2fe
Michael Wagner <michael-a.wagner@rohde-schwarz.com>
2024-01-09 17:30:54 +0800

    extract Scheduler from Core

--- termRTS.Engine/Core.cs
+++ termRTS.Engine/Core.cs
@@ -1,6 +1,12 @@
-﻿using System.Diagnostics;
+﻿namespace termRTS.Engine;
 
-namespace termRTS.Engine;
+public interface ICore
+{
+    public bool IsGameRunning();
+    public void ProcessInput();
+    public void Tick();
+    public void Render(double howFarIntoNextFrameMs);
+}
 
 // Notes to self:
 // Possible optimisation - remove all new variable allocation from game loop and replace with
@@ -19,33 +25,26 @@ namespace termRTS.Engine;
 /// <typeparam name="TComponents">
 /// Type of the enum listing all component types.
 /// </typeparam>
-internal class Core<TWorld, TComponents> where TComponents : Enum
+public class Core<TWorld, TComponents> : ICore where TComponents : Enum
 {
-    private static readonly TimeSpan MS_PER_UPDATE = TimeSpan.FromSeconds(1.0 / 60.0);
-
-    private readonly Stopwatch _stopwatch;
     private bool _isGameRunning;
     private readonly EventQueue<IEvent, ulong> _eventQueue;
 
     private TWorld _world;
     private readonly List<GameSystem<TWorld, TComponents>> _systems;
     private readonly List<GameEntity<TComponents>> _entities;
-    private readonly List<Dictionary<TComponents, IGameComponent>> _entitiesPendingChanges;
+    private readonly Dictionary<int, Dictionary<TComponents, IGameComponent>> _entitiesPendingChanges;
     private readonly List<GameEntity<TComponents>> _newEntities;
     private readonly IInput _input;
     private readonly IRenderer<TWorld, TComponents> _renderer;
-    // TODO: some form of event queue for:
-    //       - handling input
-    //       - adding/removing entities to/from the game
 
     public Core(TWorld world, IInput input, IRenderer<TWorld, TComponents> renderer)
     {
-        _stopwatch = new Stopwatch();
         _isGameRunning = false;
         _eventQueue = new EventQueue<IEvent, ulong>();
         _world = world;
         _entities = new List<GameEntity<TComponents>>();
-        _entitiesPendingChanges = new List<Dictionary<TComponents, IGameComponent>>();
+        _entitiesPendingChanges = new Dictionary<int, Dictionary<TComponents, IGameComponent>>();
         _newEntities = new List<GameEntity<TComponents>>();
         _systems = new List<GameSystem<TWorld, TComponents>>();
         _input = input;
@@ -55,6 +54,11 @@ internal class Core<TWorld, TComponents> where TComponents : Enum
     public void Shutdown()
     {
         _isGameRunning = false;
+    }
+
+    public bool IsGameRunning()
+    {
+        return _isGameRunning;
     }
 
     public void AddEntity(GameEntity<TComponents> entity)
@@ -72,96 +76,62 @@ internal class Core<TWorld, TComponents> where TComponents : Enum
         _systems.Remove(system);
     }
 
-    public void GameLoop()
+    public void ProcessInput()
     {
-        _isGameRunning = true;
-        _stopwatch.Start();
-        var lag = TimeSpan.Zero;
+    }
 
-        while (_isGameRunning)
+    public void Tick()
+    {
+        // Run game logic.
+        // NOTE: Try flipping the `for` and `foreach` loops to see which variant is faster.
+        for (var i = 0; i < _entities.Count; i += 1)
         {
-            _stopwatch.Stop();
-            lag += _stopwatch.Elapsed;
-            _stopwatch.Restart();
+            foreach (var sys in _systems)
+            {
+                var listView = _entities[..];
+                listView.RemoveAt(i);
+                var change = sys.ProcessComponents(_entities[i], listView, ref _world);
+                if (change != null)
+                {
+                    _entitiesPendingChanges[i] = change;
+                }
+            }
+        }
 
-            ProcessInput();
-
-            Update(ref lag);
-
-            Render(ref lag, out var renderElapsed);
-
-            // Take a break if we're ahead of time.
-            var loopTimeMs = lag + renderElapsed;
-
-            // If we spent longer than our allotted time, skip right ahead...
-            if (loopTimeMs >= MS_PER_UPDATE)
+        // Apply changes to the game world after all entities are done.
+        for (var i = 0; i < _entities.Count; i += 1)
+        {
+            if (!_entitiesPendingChanges.ContainsKey(i))
                 continue;
 
-            // ...otherwise wait until the next frame is due.
-            var sleepyTime = (int)(MS_PER_UPDATE - loopTimeMs).TotalMilliseconds;
-            Console.WriteLine($"pausing game loop for {sleepyTime} ms");
-            Thread.Sleep(sleepyTime);
-        }
-    }
+            var entity = _entities[i];
+            var change = _entitiesPendingChanges[i];
 
-    private void ProcessInput()
-    {
-        // TODO: Something with IInput
-    }
-
-    private void Update(ref TimeSpan lag)
-    {
-        // Advance as many steps as we need to catch up with lag.
-        while (lag >= MS_PER_UPDATE)
-        {
-            // Run game logic.
-            for (var i = 0; i < _entities.Count; i += 1)
+            foreach (var item in change)
             {
-                foreach (var sys in _systems)
-                {
-                    var listView = _entities[..];
-                    listView.RemoveAt(i);
-                    sys.ProcessComponents(_entities[i], listView, ref _world);
-                }
+                entity.Components[item.Key] = item.Value;
             }
-
-            // Apply changes to the game world after all entities are done.
-            for (var i = 0; i < _entities.Count; i += 1)
-            {
-                var entity = _entities[i];
-                var pendingChanges = _entitiesPendingChanges[i];
-                
-                if (pendingChanges.Count <= 0) continue;
-                
-                foreach (var item in pendingChanges)
-                {
-                    entity.Components[item.Key] = item.Value;
-                }
-                pendingChanges.Clear();
-            }
-
-            _entities.RemoveAll(e => e.IsMarkedForRemoval);
-            _entities.AddRange(_newEntities);
-            _newEntities.Clear();
-
-            lag -= MS_PER_UPDATE;
         }
+        _entitiesPendingChanges.Clear();
+
+        _entities.RemoveAll(e => e.IsMarkedForRemoval);
+        _entities.AddRange(_newEntities);
+        _newEntities.Clear();
+
+        // New game state:
+        //  - all pending changes cleared
+        //  - all pending new entities added
+        //  - all to-be-removed entities removed
+
     }
 
-    private void Render(ref TimeSpan lag, out TimeSpan renderElapsed)
+    public void Render(double howFarIntoNextFrameMs)
     {
-        var howFarIntoNextFrameMs = lag.TotalMilliseconds / MS_PER_UPDATE.TotalMilliseconds;
-        var renderWatch = Stopwatch.StartNew();
-
         _renderer.RenderWorld(_world, howFarIntoNextFrameMs);
 
         for (var i = 0; i < _entities.Count; i += 1)
         {
             _renderer.RenderEntity(_entities[i].Components, howFarIntoNextFrameMs);
         }
-
-        renderWatch.Stop();
-        Console.WriteLine($"render duration: {renderWatch.Elapsed}");
-        renderElapsed = renderWatch.Elapsed;
     }
 }
--- termRTS.Engine/GameSystem.cs
+++ termRTS.Engine/GameSystem.cs
@@ -14,7 +14,7 @@ public abstract class GameSystem<TWorld, TComponents> where TComponents : Enum
 {
     // TODO: Process entities and world in two-state fashion.
     // TODO: If possible, enforce that otherEs are immutable
-    public abstract void ProcessComponents(
+    public abstract Dictionary<TComponents, IGameComponent>? ProcessComponents(
             GameEntity<TComponents> thisEntityComponents,
             List<GameEntity<TComponents>> otherEntityComponents,
             ref TWorld world);
