a14277ad9947d6679357aeb517c80dd6c99ae024
micutio <wagner.mchl@googlemail.com>
2024-11-06 00:25:18 +0800

    add Voronoi map world generator

--- TermRTS.Examples/Greenery/Ecs.cs
+++ TermRTS.Examples/Greenery/Ecs.cs
@@ -1,32 +1,15 @@
 namespace TermRTS.Examples.Greenery;
 
-public class WorldComponent : ComponentBase
+public class WorldComponent(int entityId, int worldWidth, int worldHeight, byte[,] cells)
+    : ComponentBase(entityId)
 {
-    #region Fields
-    
-    private readonly Random _rng;
-    
-    #endregion
-    
     #region Constructor
-    
+
     // TODO: Find out efficient matrix dimensions and iteration!
-    public WorldComponent(int entityId, int width, int height) : base(entityId)
-    {
-        Width = width;
-        Height = height;
-        Cells = new byte[width, height];
-        _rng = new Random();
-        
-        for (var y = 0; y < Height; y++)
-        for (var x = 0; x < Width; x++)
-            Cells[x, y] = (byte)_rng.Next(0, 10);
-        // Cells[x, y] = Convert.ToByte((x + y) % 255);
-    }
-    
+
     #endregion
-    
-    public byte[,] Cells { get; }
-    public int Width { get; }
-    public int Height { get; }
+
+    public byte[,] Cells { get; } = cells;
+    public int Width { get; } = worldWidth;
+    public int Height { get; } = worldHeight;
 }
