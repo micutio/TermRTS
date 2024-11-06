1049f41279b5f820e25b8eb63cca32380c61e52d
micutio <wagner.mchl@googlemail.com>
2024-11-06 17:18:10 +0800

    add wiggle factor to voronoi cell generation

--- TermRTS.Examples/Greenery/Greenery.cs
+++ TermRTS.Examples/Greenery/Greenery.cs
@@ -6,17 +6,18 @@ namespace TermRTS.Examples.Greenery;
 public class Greenery : IRunnableExample
 {
     private readonly ILog _log;
-
+    
     public Greenery()
     {
         _log = LogManager.GetLogger(GetType());
     }
-
+    
     public void Run()
     {
         _log.Info("~ Greenery ~");
         var rng = new Random();
-
+        var seed = 0; // rng.Next();
+        
         var viewportWidth = Console.WindowWidth;
         var viewportHeight = Console.WindowHeight;
         var worldWidth = 300;
@@ -24,10 +25,10 @@ public class Greenery : IRunnableExample
         // Set up engine
         var renderer = new Renderer(viewportWidth, viewportHeight, worldWidth, worldHeight);
         var core = new Core(renderer);
-
+        
         // TODO: Move entity generation elsewhere.
         // TODO: Add 'enter text' component.
-        var worldGen = new VoronoiWorld(400, rng.Next());
+        var worldGen = new VoronoiWorld(400, 2, seed);
         var worldEntity = new EntityBase();
         var worldComponent =
             new WorldComponent(
@@ -37,34 +38,34 @@ public class Greenery : IRunnableExample
                 worldGen.Generate(worldWidth, worldHeight));
         core.AddEntity(worldEntity);
         core.AddComponent(worldComponent);
-
+        
         var scheduler = new Scheduler(16, 16, core);
         scheduler.AddEventSources(scheduler.ProfileEventReader);
         scheduler.AddEventSink(renderer, EventType.Profile);
-
+        
         // Init input
         var input = new ConsoleInput();
         scheduler.AddEventSources(input.KeyEventReader);
         scheduler.AddEventSink(input, EventType.Shutdown);
         scheduler.AddEventSink(renderer, EventType.KeyInput);
         input.Run();
-
+        
         // Graceful shutdown on canceling via CTRL+C.
         Console.CancelKeyPress += delegate(object? _, ConsoleCancelEventArgs e)
         {
             e.Cancel = true;
             scheduler.EnqueueEvent((new PlainEvent(EventType.Shutdown), 0L));
         };
-
+        
         // Automatically shut down after 10 minutes.
         scheduler.EnqueueEvent((new PlainEvent(EventType.Shutdown), 1000 * 60 * 10));
-
+        
         // Run it
         scheduler.SimulationLoop();
-
+        
         // After the app is terminated, clear the console.
         Console.Clear();
-
+        
         _log.Info("~ Greenery app shutdown ~");
     }
 }
--- TermRTS.Examples/Greenery/WorldGen.cs
+++ TermRTS.Examples/Greenery/WorldGen.cs
@@ -5,47 +5,48 @@ public interface IWorldGen
     public byte[,] Generate(int worldWidth, int worldHeight);
 }
 
-public class VoronoiWorld(int cellCount, int seed = 0) : IWorldGen
+public class VoronoiWorld(int cellCount, int jiggle, int seed = 0) : IWorldGen
 {
-    private readonly int _cellCount = cellCount;
     private readonly Random _rng = new(seed);
-
+    
     public byte[,] Generate(int worldWidth, int worldHeight)
     {
         var cells = new int[worldWidth, worldHeight];
-
+        
         // step 1: randomly sample <cellCount> coordinates of the grid as voronoi cell seeds
-        var voronois = new int[_cellCount];
-        for (var i = 0; i < _cellCount; i += 1) voronois[i] = _rng.Next(worldWidth * worldHeight);
-
-        var landWaterMap = _rng.GetItems([3, 4], _cellCount);
-
+        var voronois = new int[cellCount];
+        for (var i = 0; i < cellCount; i += 1) voronois[i] = _rng.Next(worldWidth * worldHeight);
+        
+        var landWaterMap = _rng.GetItems([3, 4], cellCount);
+        
         // step 2: associate each grid cell to one of the cell seeds
         // step 3: for each voronoi cell, determine whether it's going to be water or land
-
+        
         for (var y = 0; y < worldHeight; y += 1)
         for (var x = 0; x < worldWidth; x += 1)
         {
             var minDist = int.MaxValue;
-            for (var i = 0; i < _cellCount; i += 1)
+            for (var i = 0; i < cellCount; i += 1)
             {
                 var vX = voronois[i] % worldWidth;
                 var vY = voronois[i] / worldWidth;
                 var dist = (int)Math.Sqrt(Math.Pow(vX - x, 2) + Math.Pow(vY - y, 2));
+                var jiggleVal = _rng.Next(jiggle * -1, jiggle);
+                dist += jiggleVal;
                 if (dist >= minDist) continue;
-
+                
                 minDist = dist;
                 cells[x, y] = landWaterMap[i];
             }
         }
-
+        
         // step 4: for each voronoi land cell, apply perlin or simplex noise to generate height
-
+        
         // step 5: for each voronoi water cell, apply perlin or simplex noise to generate depth
-
+        
         // optional: cluster multiple voronoi cells to generate convex land/water shapes
         // optional: apply more techniques from "around the world" to get more appealing shapes
-
+        
         var world = new byte[worldWidth, worldHeight];
         for (var y = 0; y < worldHeight; y += 1)
         for (var x = 0; x < worldWidth; x += 1)
