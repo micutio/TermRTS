2ac6b326aa90ca5fcf5e3b2e21fc0a6012e50b99
micutio <wagner.mchl@googlemail.com>
2024-11-08 00:27:30 +0800

    WIP sloping towards coastline

--- TermRTS.Examples/Greenery/WorldGen.cs
+++ TermRTS.Examples/Greenery/WorldGen.cs
@@ -10,22 +10,24 @@ public interface IWorldGen
 public class VoronoiWorld(int cellCount, int jiggle, int seed = 0) : IWorldGen
 {
     private readonly Random _rng = new(seed);
-    
+
     public byte[,] Generate(int worldWidth, int worldHeight)
     {
         Noise.Seed = seed;
-        var cells = new int[worldWidth, worldHeight];
-        
+
         // step 1: randomly sample <cellCount> coordinates of the grid as voronoi cell seeds
         var voronois = new int[cellCount];
         for (var i = 0; i < cellCount; i += 1) voronois[i] = _rng.Next(worldWidth * worldHeight);
-        
+
         var landWaterMap = _rng.GetItems([3, 4], cellCount);
         var jiggleNoiseField = Noise.Calc2D(worldWidth, worldHeight, 0.1f);
-        
+
         // step 2: associate each grid cell to one of the cell seeds
         // step 3: for each voronoi cell, determine whether it's going to be water or land
-        
+
+        var cellHeights = new int[worldWidth, worldHeight];
+        var heightFactors = new double[worldWidth, worldHeight];
+
         for (var y = 0; y < worldHeight; y += 1)
         for (var x = 0; x < worldWidth; x += 1)
         {
@@ -34,16 +36,21 @@ public class VoronoiWorld(int cellCount, int jiggle, int seed = 0) : IWorldGen
             {
                 var vX = voronois[i] % worldWidth;
                 var vY = voronois[i] / worldWidth;
-                var dist = (int)Math.Sqrt(Math.Pow(vX - x, 2) + Math.Pow(vY - y, 2));
-                var jiggleVal = (int)(jiggleNoiseField[x, y] / 255 * jiggle);
+                var dist = Math.Sqrt(Math.Pow(vX - x, 2.0f) + Math.Pow(vY - y, 2.0f));
+                var jiggleVal = jiggleNoiseField[x, y] / 255.0f * jiggle;
                 dist += jiggleVal;
-                if (dist >= minDist) continue;
-                
-                minDist = dist;
-                cells[x, y] = landWaterMap[i];
+
+                // Gauge the distance to the shoreline by how close to equidistant we are between
+                // two voronoi cell centres. Normalise and use this as multiplier for height.
+                heightFactors[x, y] = Math.Min(9.0f, Math.Abs(dist - minDist)) / 9.0f;
+
+                if (dist > minDist) continue;
+
+                minDist = (int)dist;
+                cellHeights[x, y] = landWaterMap[i];
             }
         }
-        
+
         // step 4: for each voronoi land cell, apply perlin or simplex noise to generate height
         Noise.Seed = seed;
         const float noiseScale = 0.1f;
@@ -51,19 +58,20 @@ public class VoronoiWorld(int cellCount, int jiggle, int seed = 0) : IWorldGen
         for (var y = 0; y < worldHeight; y += 1)
         for (var x = 0; x < worldWidth; x += 1)
         {
-            var factor = cells[x, y] == 4 ? 1 : -1;
+            var factor = cellHeights[x, y] == 4 ? 1 : -1;
             var noiseVal = (int)(noiseField[x, y] / 255.0f * 5.0f);
-            cells[x, y] = Math.Clamp(cells[x, y] + noiseVal * factor, 0, 9);
+            cellHeights[x, y] =
+                (int)Math.Clamp(cellHeights[x, y] + noiseVal * factor * heightFactors[x, y], 0, 9);
         }
         // step 5: for each voronoi water cell, apply perlin or simplex noise to generate depth
-        
+
         // optional: cluster multiple voronoi cells to generate convex land/water shapes
         // optional: apply more techniques from "around the world" to get more appealing shapes
-        
+
         var world = new byte[worldWidth, worldHeight];
         for (var y = 0; y < worldHeight; y += 1)
         for (var x = 0; x < worldWidth; x += 1)
-            world[x, y] = Convert.ToByte(cells[x, y]);
+            world[x, y] = Convert.ToByte(cellHeights[x, y]);
         return world;
     }
 }
