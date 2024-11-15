956310fc90fc120423ae141683cbba27832d0f38
micutio <wagner.mchl@googlemail.com>
2024-11-15 20:36:58 +0800

    move TextBox.cs into separate folder, add class file for Parser

---
+++ TermRTS.Examples/Greenery/Command/Parser.cs
@@ -0,0 +1,13 @@
+﻿namespace TermRTS.Examples.Greenery.Command;
+
+// TODO: Parse text input into tokens.
+// TODO: Return error message for invalid outputs.
+// TODO: Think about UI for text input and output.
+
+/// <summary>
+/// Parses text to commands
+///  - or tokens?
+/// </summary>
+public class Parser
+{
+}
