b9eddfae6e1ee380ef9957ee56664eee29e7355c
micutio <wagner.mchl@googlemail.com>
2024-12-03 17:34:47 +0800

    draft CommandRunner structure

---
+++ TermRTS.Examples/Greenery/Command/CommandRunner.cs
@@ -0,0 +1,20 @@
+﻿namespace TermRTS.Examples.Greenery.Command;
+
+public class CommandRunner
+{
+    private const string ErrorEmptyCmd = "< Cannot run empty command";
+    private const string ErrorNoIdentifier = "< Command must start with an identifier!";
+    private const string ErrorUnknownCmd = "< Unknown command!";
+    
+    public string Run(IReadOnlyList<Token> cmdTokens)
+    {
+        if (cmdTokens.Count == 0) return ErrorEmptyCmd;
+        
+        if (cmdTokens[0].TokenType != TokenType.Identifier) return ErrorNoIdentifier;
+        
+        return cmdTokens[0].Lexeme switch
+        {
+            
+        }
+    }
+}
