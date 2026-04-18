7fd77fd1018f66ae43c6f0dab788db2be933cd08
micutio <wagner.mchl@googlemail.com>
2026-04-18 20:09:20 +0800

    WIP packed data: implement packing and storing world gen results

--- TermRTS.Examples/Greenery/WorldGen/WorldPacker.cs
+++ TermRTS.Examples/Greenery/WorldGen/WorldPacker.cs
@@ -2,30 +2,74 @@ using System.Runtime.InteropServices;
 
 namespace TermRTS.Examples.Greenery.WorldGen;
 
+public static class VisibilityMasks
+{
+    public const byte InFOV = 1 << 0; // 0000_0001
+    public const byte ExploredFOW = 1 << 1; // 0000_0010
+}
+
+public static class PackedAttribute
+{
+    public const int BiomeShift = 0;
+    public const int ElevShift = 8;
+    public const int HumidShift = 16;
+    public const int FeatShift = 24;
+}
+
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct PackedTile
 {
-    internal const int BiomeShift = 0;
-    internal const int ElevShift = 8;
-    internal const int HumidShift = 16;
-    internal const int FeatShift = 24;
+    #region Fields
 
-    // 4 bytes: Biome, Elevation, Humidity, Surface Feature
+    // 4 Bytes: Biome, Elevation, Humidity, Surface Feature
     public uint PackedAttributes;
 
-    // 4 bytes
-    public float Temperature;
+    // 1 Byte (-128 to +127 Celsius) 
+    public sbyte Temperature;
 
-    // 4 bytes: 2 vectors (X, Y) using 1 byte per component
-    public sbyte FlowX;
-    public sbyte FlowY;
-    public sbyte WindX;
-    public sbyte WindY;
+    // 1 Byte (FOV, FOW, and six more bools)
+    public byte VisibilityFlags;
 
-    public Biome Biome => (Biome)(PackedAttributes & 0xFF);
-    public byte Elevation => (byte)(PackedAttributes >> ElevShift & 0xFF);
-    public byte Humidity => (byte)(PackedAttributes >> HumidShift & 0xFF);
-    public SurfaceFeature SurfaceFeature => (SurfaceFeature)(PackedAttributes >> FeatShift & 0xFF);
+    // 1 Byte (Holds FlowX, FlowY, WindX, WindY)
+    public byte PackedVectors;
+
+    // 1 Byte padding to hit exactly 8 Bytes. Usable for more fields.
+    public byte Reserved;
+
+    #endregion
+
+    #region Properties
+
+    public Biome Biome => (Biome)(PackedAttributes >> PackedAttribute.BiomeShift & 0xFF);
+    public byte Elevation => (byte)(PackedAttributes >> PackedAttribute.ElevShift & 0xFF);
+    public byte Humidity => (byte)(PackedAttributes >> PackedAttribute.HumidShift & 0xFF);
+
+    public SurfaceFeature SurfaceFeature =>
+        (SurfaceFeature)(PackedAttributes >> PackedAttribute.FeatShift & 0xFF);
+
+    public bool IsVisible => (VisibilityFlags & VisibilityMasks.InFOV) != 0;
+    public bool IsExplored => (VisibilityFlags & VisibilityMasks.ExploredFOW) != 0;
+
+    #endregion
+
+    #region Setters
+
+    public void SetInFov()
+    {
+        VisibilityFlags &= VisibilityMasks.InFOV;
+    }
+
+    public void SetOutOfFov()
+    {
+        VisibilityFlags &= unchecked((byte)~VisibilityMasks.InFOV);
+    }
+
+    public void SetExplored()
+    {
+        VisibilityFlags |= VisibilityMasks.InFOV;
+    }
+
+    #endregion
 }
 
 /// <summary>
@@ -47,48 +91,28 @@ public class WorldPacker
 
         for (var i = 0; i < length; i++)
         {
-            var attr = (uint)biomes[i] << PackedTile.BiomeShift |
-                       (uint)elevations[i] << PackedTile.ElevShift |
-                       (uint)humidities[i] << PackedTile.HumidShift |
-                       (uint)features[i] << PackedTile.FeatShift;
+            var attr = (uint)biomes[i] << PackedAttribute.BiomeShift |
+                       (uint)elevations[i] << PackedAttribute.ElevShift |
+                       (uint)humidities[i] << PackedAttribute.HumidShift |
+                       (uint)features[i] << PackedAttribute.FeatShift;
+
+            var visibilityFlags = (byte)0;
+
+            var packedVectors = (byte)(
+                waterflows[i].x + 1 & 0x3 |
+                (waterflows[i].y + 1 & 0x3) << 2 |
+                (winds[i].x + 1 & 0x3) << 4 |
+                (winds[i].y + 1 & 0x3) << 6);
 
             packedMap[i] = new PackedTile
             {
                 PackedAttributes = attr,
-                Temperature = temperatures[i],
-                FlowX = (sbyte)waterflows[i].x,
-                FlowY = (sbyte)waterflows[i].y,
-                WindX = (sbyte)winds[i].x,
-                WindY = (sbyte)winds[i].y
+                Temperature = Convert.ToSByte(temperatures[i]),
+                VisibilityFlags = visibilityFlags,
+                PackedVectors = packedVectors
             };
         }
 
         return packedMap;
-    }
-
-    public static void Unpack(
-        ReadOnlySpan<PackedTile> packedData,
-        Span<byte> biomes,
-        Span<byte> elevations,
-        Span<float> temperatures,
-        Span<byte> humidities,
-        Span<(int x, int y)> waterflows,
-        Span<(int x, int y)> winds,
-        Span<byte> features)
-    {
-        for (var i = 0; i < packedData.Length; i++)
-        {
-            ref readonly var tile = ref packedData[i];
-            var attr = tile.PackedAttributes;
-
-            biomes[i] = (byte)(attr & 0xFF);
-            elevations[i] = (byte)(attr >> PackedTile.ElevShift & 0xFF);
-            humidities[i] = (byte)(attr >> PackedTile.HumidShift & 0xFF);
-            features[i] = (byte)(attr >> PackedTile.FeatShift & 0xFF);
-
-            temperatures[i] = tile.Temperature;
-            waterflows[i] = (tile.FlowX, tile.FlowY);
-            winds[i] = (tile.WindX, tile.WindY);
-        }
     }
 }
