642c7a64cb237ba6b546a7c47781f9cfb6994e7c
micutio <wagner.mchl@googlemail.com>
2026-04-19 11:17:51 +0800

    add packing tests and performance improvements

---
+++ TermRTS.Examples.Test/WorldPackerTests.cs
@@ -0,0 +1,224 @@
+using System;
+using TermRTS.Examples.Greenery.WorldGen;
+using Xunit;
+
+namespace TermRTS.Examples.Test;
+
+public class WorldPackerTests
+{
+    [Fact]
+    public void Pack_Unpack_RetainsAllFields()
+    {
+        var length = 64;
+        var biomes = new Biome[length];
+        var elevations = new byte[length];
+        var humidities = new byte[length];
+        var temperatures = new float[length];
+        var waterflows = new (int x, int y)[length];
+        var winds = new (int x, int y)[length];
+        var features = new SurfaceFeature[length];
+
+        var random = new Random(1234);
+        for (var i = 0; i < length; i++)
+        {
+            biomes[i] = (Biome)random.Next(Enum.GetValues<Biome>().Length);
+            elevations[i] = (byte)random.Next(0, 255);
+            humidities[i] = (byte)random.Next(0, 100);
+            temperatures[i] = random.Next(-40, 40);
+            waterflows[i] = (
+                random.Next(-1, 2),
+                random.Next(-1, 2));
+            winds[i] = (
+                random.Next(-1, 2),
+                random.Next(-1, 2));
+            features[i] = (SurfaceFeature)random.Next(Enum.GetValues<SurfaceFeature>().Length);
+        }
+
+        var packed = WorldPacker.Pack(
+            biomes,
+            elevations,
+            temperatures,
+            humidities,
+            waterflows,
+            winds,
+            features);
+
+        Assert.Equal(length, packed.Length);
+
+        for (var i = 0; i < length; i++)
+        {
+            WorldPacker.Unpack(
+                packed[i],
+                out var biome,
+                out var elevation,
+                out var humidity,
+                out var temperature,
+                out var waterflow,
+                out var wind,
+                out var feature);
+
+            Assert.Equal(biomes[i], biome);
+            Assert.Equal(elevations[i], elevation);
+            Assert.Equal(humidities[i], humidity);
+            Assert.Equal(features[i], feature);
+            Assert.Equal(Convert.ToSByte(temperatures[i]), temperature);
+            Assert.Equal(waterflows[i], waterflow);
+            Assert.Equal(winds[i], wind);
+        }
+    }
+
+    [Fact]
+    public void Pack_BitsArePackedCorrectly_ForEdgeValues()
+    {
+        var biomes = new Biome[] { Biome.HighSeas, Biome.MajorRiver };
+        var elevations = new byte[] { 0, 255 };
+        var temperatures = new float[] { -40, 127 };
+        var humidities = new byte[] { 0, 100 };
+        var waterflows = new[] { (x: -1, y: -1), (x: 1, y: 1) };
+        var winds = new[] { (x: -1, y: 1), (x: 1, y: -1) };
+        var features = new[] { SurfaceFeature.None, SurfaceFeature.Snow };
+
+        var packed = WorldPacker.Pack(
+            biomes,
+            elevations,
+            temperatures,
+            humidities,
+            waterflows,
+            winds,
+            features);
+
+        Assert.Equal((byte)0, packed[0].PackedVectors);
+        Assert.Equal((byte)255, packed[1].PackedVectors);
+
+        Assert.Equal(Biome.HighSeas, packed[0].Biome);
+        Assert.Equal(Biome.MajorRiver, packed[1].Biome);
+        Assert.Equal(0, packed[0].Elevation);
+        Assert.Equal(255, packed[1].Elevation);
+        Assert.Equal(SurfaceFeature.None, packed[0].SurfaceFeature);
+        Assert.Equal(SurfaceFeature.Snow, packed[1].SurfaceFeature);
+
+        Assert.Equal((-1, -1), packed[0].WaterFlow);
+        Assert.Equal((1, 1), packed[1].WaterFlow);
+        Assert.Equal((-1, 1), packed[0].Wind);
+        Assert.Equal((1, -1), packed[1].Wind);
+    }
+
+    [Fact]
+    public void Pack_ThrowsWhenArraysDifferInLength()
+    {
+        var biomes = new Biome[4];
+        var elevations = new byte[4];
+        var humidities = new byte[3];
+        var temperatures = new float[4];
+        var waterflows = new (int x, int y)[4];
+        var winds = new (int x, int y)[4];
+        var features = new SurfaceFeature[4];
+
+        Assert.Throws<ArgumentException>(() => WorldPacker.Pack(
+            biomes,
+            elevations,
+            temperatures,
+            humidities,
+            waterflows,
+            winds,
+            features));
+    }
+
+    [Fact]
+    public void Pack_ThrowsWhenDirectionsAreOutOfRange()
+    {
+        var biomes = new Biome[2];
+        var elevations = new byte[2];
+        var humidities = new byte[2];
+        var temperatures = new float[2];
+        var waterflows = new[] { (x: -2, y: 0), (x: 0, y: 0) };
+        var winds = new[] { (x: 0, y: 0), (x: 0, y: 0) };
+        var features = new SurfaceFeature[2];
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => WorldPacker.Pack(
+            biomes,
+            elevations,
+            temperatures,
+            humidities,
+            waterflows,
+            winds,
+            features));
+    }
+
+    [Fact]
+    public void WorldGenerator_ProducesPackedChunksCompatibleWithEcs()
+    {
+        var worldGen = new CylinderWorld(
+            WorldMath.WorldWidth,
+            WorldMath.WorldHeight,
+            0.35f,
+            seed: 3,
+            voronoiCellCount: 175,
+            plateCount: 28,
+            new ElevationParameters(),
+            new CoastalParameters(),
+            new VolcanicParameters(),
+            new ErosionParameters(),
+            new ClimateParameters(),
+            new RiverParameters());
+
+        var result = worldGen.Generate();
+
+        var expectedChunkCount = WorldMath.ChunksAcross * (WorldMath.WorldHeight / WorldMath.ChunkSize);
+        Assert.Equal(expectedChunkCount, result.PackedData.Length);
+
+        foreach (var chunk in result.PackedData)
+        {
+            Assert.Equal(WorldMath.ChunkSize * WorldMath.ChunkSize, chunk.PackedTiles.Length);
+            Assert.InRange(chunk.Cx, 0, WorldMath.ChunksAcross - 1);
+            Assert.InRange(chunk.Cy, 0, WorldMath.WorldHeight / WorldMath.ChunkSize - 1);
+            Assert.True(chunk.PackedTiles[0].Elevation >= 0);
+        }
+    }
+
+    [Fact]
+    public void PackPooled_RentAndReturn_Works()
+    {
+        var length = 32;
+        var biomes = new Biome[length];
+        var elevations = new byte[length];
+        var humidities = new byte[length];
+        var temperatures = new float[length];
+        var waterflows = new (int x, int y)[length];
+        var winds = new (int x, int y)[length];
+        var features = new SurfaceFeature[length];
+
+        for (var i = 0; i < length; i++)
+        {
+            biomes[i] = (Biome)(i % Enum.GetValues<Biome>().Length);
+            elevations[i] = (byte)(i % 256);
+            humidities[i] = (byte)(i % 101);
+            temperatures[i] = i % 120 - 40;
+            waterflows[i] = (0, 0);
+            winds[i] = (0, 0);
+            features[i] = SurfaceFeature.None;
+        }
+
+        var buffer = WorldPacker.PackPooled(
+            biomes,
+            elevations,
+            temperatures,
+            humidities,
+            waterflows,
+            winds,
+            features);
+
+        Assert.NotNull(buffer);
+        Assert.True(buffer.Length >= length);
+
+        for (var i = 0; i < length; i++)
+        {
+            Assert.Equal(biomes[i], buffer[i].Biome);
+            Assert.Equal(elevations[i], buffer[i].Elevation);
+            Assert.Equal(humidities[i], buffer[i].Humidity);
+        }
+
+        // Return the pooled array to avoid leaking rented buffers in tests
+        WorldPacker.ReturnPackedArray(buffer, clearArray: true);
+    }
+}
--- TermRTS.Examples/Greenery/WorldGen/WorldPacker.cs
+++ TermRTS.Examples/Greenery/WorldGen/WorldPacker.cs
@@ -1,3 +1,5 @@
+using System.Buffers;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace TermRTS.Examples.Greenery.WorldGen;
@@ -24,7 +26,7 @@ public struct PackedTile
     // 4 Bytes: Biome, Elevation, Humidity, Surface Feature
     public uint PackedAttributes;
 
-    // 1 Byte (-128 to +127 Celsius) 
+    // 1 Byte (-128 to +127 Celsius)
     public sbyte Temperature;
 
     // 1 Byte (FOV, FOW, and six more bools)
@@ -50,13 +52,21 @@ public struct PackedTile
     public bool IsVisible => (VisibilityFlags & VisibilityMasks.InFOV) != 0;
     public bool IsExplored => (VisibilityFlags & VisibilityMasks.ExploredFOW) != 0;
 
+    public (int x, int y) WaterFlow => (
+        (PackedVectors >> 0 & 0x3) - 1,
+        (PackedVectors >> 2 & 0x3) - 1);
+
+    public (int x, int y) Wind => (
+        (PackedVectors >> 4 & 0x3) - 1,
+        (PackedVectors >> 6 & 0x3) - 1);
+
     #endregion
 
     #region Setters
 
     public void SetInFov()
     {
-        VisibilityFlags &= VisibilityMasks.InFOV;
+        VisibilityFlags |= VisibilityMasks.InFOV;
     }
 
     public void SetOutOfFov()
@@ -66,7 +76,7 @@ public struct PackedTile
 
     public void SetExplored()
     {
-        VisibilityFlags |= VisibilityMasks.InFOV;
+        VisibilityFlags |= VisibilityMasks.ExploredFOW;
     }
 
     #endregion
@@ -75,7 +85,7 @@ public struct PackedTile
 /// <summary>
 /// Packs multiple world data sets into one single packed array.
 /// </summary>
-public class WorldPacker
+public static class WorldPacker
 {
     public static PackedTile[] Pack(
         ReadOnlySpan<Biome> biomes,
@@ -87,32 +97,155 @@ public class WorldPacker
         ReadOnlySpan<SurfaceFeature> features)
     {
         var length = biomes.Length;
+        if (length != elevations.Length || length != temperatures.Length ||
+            length != humidities.Length || length != waterflows.Length ||
+            length != winds.Length || length != features.Length)
+        {
+            throw new ArgumentException("All input spans must have the same length.");
+        }
+
         var packedMap = new PackedTile[length];
 
-        for (var i = 0; i < length; i++)
+        for (var i = 0; i < packedMap.Length; i++)
         {
-            var attr = (uint)biomes[i] << PackedAttribute.BiomeShift |
-                       (uint)elevations[i] << PackedAttribute.ElevShift |
-                       (uint)humidities[i] << PackedAttribute.HumidShift |
-                       (uint)features[i] << PackedAttribute.FeatShift;
-
-            var visibilityFlags = (byte)0;
-
-            var packedVectors = (byte)(
-                waterflows[i].x + 1 & 0x3 |
-                (waterflows[i].y + 1 & 0x3) << 2 |
-                (winds[i].x + 1 & 0x3) << 4 |
-                (winds[i].y + 1 & 0x3) << 6);
-
             packedMap[i] = new PackedTile
             {
-                PackedAttributes = attr,
-                Temperature = Convert.ToSByte(temperatures[i]),
-                VisibilityFlags = visibilityFlags,
-                PackedVectors = packedVectors
+                PackedAttributes = PackAttributes(
+                    biomes[i],
+                    elevations[i],
+                    humidities[i],
+                    features[i]),
+                Temperature = PackTemperature(temperatures[i]),
+                VisibilityFlags = 0,
+                PackedVectors = PackVectors(waterflows[i], winds[i])
             };
         }
 
         return packedMap;
+    }
+
+    public static void Unpack(
+        PackedTile packed,
+        out Biome biome,
+        out byte elevation,
+        out byte humidity,
+        out sbyte temperature,
+        out (int x, int y) waterflow,
+        out (int x, int y) wind,
+        out SurfaceFeature feature)
+    {
+        biome = packed.Biome;
+        elevation = packed.Elevation;
+        humidity = packed.Humidity;
+        temperature = packed.Temperature;
+        feature = packed.SurfaceFeature;
+        waterflow = packed.WaterFlow;
+        wind = packed.Wind;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint PackAttributes(
+        Biome biome,
+        byte elevation,
+        byte humidity,
+        SurfaceFeature feature)
+    {
+        return (uint)biome << PackedAttribute.BiomeShift |
+               (uint)elevation << PackedAttribute.ElevShift |
+               (uint)humidity << PackedAttribute.HumidShift |
+               (uint)feature << PackedAttribute.FeatShift;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static sbyte PackTemperature(float temperature)
+    {
+        // Clamp instead of throwing to avoid exceptions on occasional out-of-range
+        // values. This keeps the hot path exception-free and predictable.
+        var clamped = temperature;
+        if (clamped < sbyte.MinValue) clamped = sbyte.MinValue;
+        else if (clamped > sbyte.MaxValue) clamped = sbyte.MaxValue;
+
+        return Convert.ToSByte(clamped);
+    }
+
+    /// <summary>
+    /// Packs into a pooled array rented from <see cref="ArrayPool{PackedTile}.Shared"/>.
+    /// Caller must return the array with <see cref="ReturnPackedArray(PackedTile[],bool)"/> when done.
+    /// The returned array length may be larger than the requested length; only the first
+    /// <paramref name="biomes"/>.Length elements are valid.
+    /// </summary>
+    public static PackedTile[] PackPooled(
+        ReadOnlySpan<Biome> biomes,
+        ReadOnlySpan<byte> elevations,
+        ReadOnlySpan<float> temperatures,
+        ReadOnlySpan<byte> humidities,
+        ReadOnlySpan<(int x, int y)> waterflows,
+        ReadOnlySpan<(int x, int y)> winds,
+        ReadOnlySpan<SurfaceFeature> features)
+    {
+        var length = biomes.Length;
+        if (length != elevations.Length || length != temperatures.Length ||
+            length != humidities.Length || length != waterflows.Length ||
+            length != winds.Length || length != features.Length)
+        {
+            throw new ArgumentException("All input spans must have the same length.");
+        }
+
+        var pool = ArrayPool<PackedTile>.Shared;
+        var buffer = pool.Rent(length);
+
+        for (var i = 0; i < length; i++)
+        {
+            buffer[i] = new PackedTile
+            {
+                PackedAttributes = PackAttributes(
+                    biomes[i],
+                    elevations[i],
+                    humidities[i],
+                    features[i]),
+                Temperature = PackTemperature(temperatures[i]),
+                VisibilityFlags = 0,
+                PackedVectors = PackVectors(waterflows[i], winds[i])
+            };
+        }
+
+        return buffer;
+    }
+
+    /// <summary>
+    /// Return a pooled array obtained from <see cref="PackPooled"/> back to the shared pool.
+    /// If <paramref name="clearArray"/> is true the array will be cleared before returning.
+    /// </summary>
+    public static void ReturnPackedArray(PackedTile[] buffer, bool clearArray = false)
+    {
+        if (buffer is null) return;
+        ArrayPool<PackedTile>.Shared.Return(buffer, clearArray);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static byte PackVectors(
+        (int x, int y) waterflow,
+        (int x, int y) wind)
+    {
+        ValidateDirection(waterflow.x, nameof(waterflow));
+        ValidateDirection(waterflow.y, nameof(waterflow));
+        ValidateDirection(wind.x, nameof(wind));
+        ValidateDirection(wind.y, nameof(wind));
+
+        return (byte)(
+            (waterflow.x + 1 & 0x3) |
+            ((waterflow.y + 1 & 0x3) << 2) |
+            ((wind.x + 1 & 0x3) << 4) |
+            ((wind.y + 1 & 0x3) << 6));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void ValidateDirection(int direction, string name)
+    {
+        if (direction is < -1 or > 1)
+        {
+            throw new ArgumentOutOfRangeException(name,
+                "Direction values must be -1, 0 or 1.");
+        }
     }
 }
